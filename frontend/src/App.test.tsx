4bfa5a03bb1a6e3fca7c017a898b94f82b6a2373
Kyle Hrabusa <hrabusa.4@osu.edu>
2023-11-27 10:18:55 -0500

    pgs 10+11

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -4,14 +4,24 @@ import App from './App';
 
 describe('The main app',() => {
 
-  it('should render the Orders Link',() => {
+  it('should render the Orders link',() => {
     render(<App />);
     expect(screen.getByText('Orders')).toBeInTheDocument();
   });
 
-  it('should render the Payments Link',() => {
+  it('should render the Payments link',() => {
     render(<App />);
     expect(screen.getByText('Payments')).toBeInTheDocument();
+  });
+
+  it('should render the Home link',() => {
+    render(<App />);
+    expect(screen.getAllByRole('link', { name: "Home" })).toBeInTheDocument();
+  });
+
+  it('should render the Home header',() => {
+    render(<App />);
+    expect(screen.getAllByRole('heading', { level: 2 })).toBeInTheDocument();
   });
 
 });
